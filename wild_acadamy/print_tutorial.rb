474db1b034d8d3c9921e1ee2d3fedfd2e2b5cf11
John Gilmore <johngilmore@RS1063.local>
2015-11-19 15:01:42 -0800

    initial printing tutorial

---
+++ wild_acadamy/print_tutorial.rb
@@ -0,0 +1,4 @@
+print "print line1" #prints line with out carriage return
+print "print line2" #prints line with out carriage return
+puts  "puts line3"  #prints line with carriage return
+puts  "puts line4"  #prints line with carriage return

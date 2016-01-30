330bd1b372d15597e1d0fca785d972ff09c4bb1f
John Gilmore <johngilmore@RS1063.local>
2016-01-29 18:04:16 -0800

    adding arithmatic operator tutorial

---
+++ wild_acadamy/arithmatic_operators.rb
@@ -0,0 +1,30 @@
+# Add, Subtract, Multiply, Divide, Exponential, Modulo
+
+# ADD +
+puts "how much is 6 + 7?"
+puts 6 + 7
+
+# SUBTRACT -
+puts "how much is 20 - 10?"
+puts 20 - 10
+
+
+# MULTIPLY *
+puts "how much is 8 * 10?"
+puts 8 * 10
+
+
+# DIVIDE /
+puts "how much is 100 / 10?"
+puts 100 / 10
+
+
+#EXPONENTIAL **
+puts "how much is 10 ** 4?"
+puts 10 ** 4
+
+
+#MODULO %
+puts "how much is 15 % 4? this calulates the remainder"
+puts 15 % 4
+

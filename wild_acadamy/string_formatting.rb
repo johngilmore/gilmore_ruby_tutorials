c6daeef96f60f5452916d9efadb421049b968877
John Gilmore <johngilmore@RS1063.local>
2016-02-05 13:51:38 -0800

    this is the partially finished string_formatting tutorial

---
+++ wild_acadamy/string_formatting.rb
@@ -0,0 +1,16 @@
+# Lesson4 - Formatting: capitalize, upcase, downcase, reverse, length
+
+my_name = "johngilmore"
+
+#STRING_VARIABLE
+puts my_name
+puts my_name.capitalize
+puts my_name.upcase
+puts my_name.downcase
+puts my_name.reverse
+puts my_name.length
+$end
+
+## WORK IN-PROGRESS. HALFWAY THRU TUTORIAL
+
+

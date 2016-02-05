11825a1e76f955a6c385f486745ca5ef93b6b488
John Gilmore <johngilmore@RS1063.local>
2016-02-05 13:24:28 -0800

    adding if_else_elsif tutorial and enhancing the print_tutorial

---
+++ wild_acadamy/if_else_elsif.rb
@@ -0,0 +1,21 @@
+# TUTORIAL 3 - IF,ELSE,ELSIF
+
+my_age = 100
+
+if my_age <= 17
+
+  puts "Age: #{my_age}"
+  puts "too young"
+
+else
+
+  puts "Age: #{my_age}"
+  puts "what is your gender?"
+
+end
+
+# my_age = 17 --> waht is you genger
+# my_age = 19 --> too young
+# my_age = 0  --> too young
+
+
--- wild_acadamy/print_tutorial.rb
+++ wild_acadamy/print_tutorial.rb
@@ -1,5 +1,18 @@
+#OUTPUT TUTORIAL
+
+#PRINT VS PUTS
 print "print line1" #results in line with out carriage return
 print "print line2" #results in line with out carriage return
 puts  "puts line3"  #results in line with carriage return AT END
 puts  "puts line4"  #results in line with carriage return AT END
 print "print line5" #results in line w/out carriage return. this will be on a new line due to the PUTS above
+
+#PRINT CARRIAGE RETURN
+print "\n"
+
+
+#OUTPUTING STRING AND VARIABLE
+string_variable = "FOOBAR"
+puts "This PUTS statement outputs the string_variables value #{string_variable}"
+
+
